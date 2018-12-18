5fa3ff3faa840494edec5fefa53ac75b3bb7a35d
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2018-12-18 16:56:06 +0800

    In the middle of refactoring

--- src/download.py
+++ src/download.py
@@ -5,6 +5,7 @@ import argparse
 import logging
 import time
 import sqlite3
+import db
 import subprocess
 from pytube import YouTube
 
@@ -19,10 +20,10 @@ def update_true_fps(filename, filepath, sqlite_conn, conn_cursor):  # YT does no
 
     reg = re.search(r'\d+\.?\d?\d? fps', str(err))
     fps = float(reg.group().split(' ')[0])
-    conn_cursor.execute('UPDATE metadata SET fps=? WHERE video_id=?', (fps,filename))
-    sqlite_conn.commit()
+    #conn_cursor.execute('UPDATE metadata SET fps=? WHERE video_id=?', (fps,filename))
+    #sqlite_conn.commit()
 
-def record_stream_data(yt, stream, filename, start, end, sqlite_conn, conn_cursor):
+def record_stream_data(yt, stream, filename, start, end, db_conn):
     '''start and end are the start and end times in seconds as strings
     '''
     vid = filename
@@ -42,8 +43,9 @@ def record_stream_data(yt, stream, filename, start, end, sqlite_conn, conn_curso
     elif end < start and end != -1:
         raise ValueError('End time of {} is before start time of {} for {} with id {}.'.format(end, start, title, vid))
     data = (vid, title, fps, abr, res, kw, itag, mime, length, cap, start, end)
-    conn_cursor.execute('''INSERT INTO metadata (video_id, title, fps, abr, resolution, keywords, itag, mime_type, full_length_sec, captions, start_time, end_time) VALUES ({})'''.format('?,'*11+'?'), data)
-    sqlite_conn.commit()
+    db_conn.insert('metadata', 'metadata', ('video_id', 'title', 'fps', 'abr', 'resolution', 'keywords', 'itag', 'mime_type', 'full_length_sec', 'captions', 'start_time', 'end_time'), data)
+    #conn_cursor.execute('''INSERT INTO metadata (video_id, title, fps, abr, resolution, keywords, itag, mime_type, full_length_sec, captions, start_time, end_time) VALUES ({})'''.format('?,'*11+'?'), data)
+    #sqlite_conn.commit()
 
 def is_valid_stream(yt, stream, lower_res, upper_res):
     res = stream.resolution[:-1]
@@ -59,35 +61,37 @@ def get_filename(yt, stream, start, end):
         end = stream.player_config_args['length_seconds']
     return yt.video_id + '-' + start + '-' + end
 
-def single_file_download_url(input_filename, output_dir, sqlite_conn, conn_cursor):
+def single_file_download_url(input_filename, output_dir, db_conn):
     '''Downloads videos from URL in input_file. URLs should be separated by newlines.
     '''
     with open(input_filename) as f:
         for line in f:
             link, start, end = line.strip().split(' ')  # start and end should be whole numbers representing start and end times in seconds as a string
             yt = YouTube(link)
+            not_exists = db_conn.fetchone('metadata', 'metadata', ['video_id'], ('video_id', yt.video_id)) is None
+            if not_exists:
             conn_cursor.execute('SELECT video_id FROM metadata WHERE video_id=?', (yt.video_id,))
-            if conn_cursor.fetchone() is None:  # No records of this video in metadata, proceed to download
+            #if conn_cursor.fetchone() is None:  # No records of this video in metadata, proceed to download
                 streams_by_type = [yt.streams.filter(type='video', subtype=tp).order_by('resolution').all() for tp in formats]
                 for i, streams in enumerate(streams_by_type):
                     stream = get_valid_stream(yt, streams)
                     if stream is not None:
                         #try:
                             filename = get_filename(yt, stream, start, end)
-                            record_stream_data(yt, stream, filename, start, end, sqlite_conn, conn_cursor)
+                            record_stream_data(yt, stream, filename, start, end, db_conn)
                             stream.download(output_path=output_dir, filename=filename)
-                            update_true_fps(filename, os.path.join(output_dir, filename+'.'+stream.subtype),  sqlite_conn, conn_cursor) 
+                            update_true_fps(filename, os.path.join(output_dir, filename+'.'+stream.subtype),  db_conn) 
                             logger.info('Stream for {} with video id {} has been downloaded'.format(yt.title, yt.video_id))
                             break
                         #except:
                         #    logger.error('{}, video id {}, itag {} not downloaded: connection error or something'.format(yt.title, yt.video_id, stream.itag))
                     logger.warning('{}, video id {}, subtype {} not downloaded: no valid formats'.format(yt.title, yt.video_id, formats[i]))
 
-def download_folder_url(input_dir, output_dir, sqlite_conn, conn_cursor):
+def download_folder_url(input_dir, output_dir, sqlite_conn):
     all_files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
     for filename in all_files:
         if filename != '.gitkeep':
-            single_file_download_url(os.path.join(input_folder, filename), output_dir, sqlite_conn, conn_cursor)
+            single_file_download_url(os.path.join(input_folder, filename), output_dir, sqlite_conn)
 
 if __name__ == '__main__':
     arguments = sys.argv
@@ -104,13 +108,14 @@ if __name__ == '__main__':
         parser.add_argument('folder', help='Youtube videos in files in given directory will be downloaded to data/video/unprocessed.')
         input_folder = os.path.join(proj_root, parser.parse_args().folder)
 
-    conn = sqlite3.connect(os.path.join(proj_root, 'db', 'metadata.db'))
-    c = conn.cursor()
+   # conn = sqlite3.connect(os.path.join(proj_root, 'db', 'metadata.db'))
+   # c = conn.cursor()
+    db_conn = db.DatabaseConnector(['metadata'])
 
     if os.path.isfile(input_folder):
         # user actually input a file
-        single_file_download_url(input_folder, output_dir, conn, c)
+        single_file_download_url(input_folder, output_dir, db_conn)
     else:
-        download_folder_url(input_folder, output_dir, conn, c)
+        download_folder_url(input_folder, output_dir, db_conn)
 
-    conn.close()
+    #conn.close()
--- src/initdb.py
+++ src/initdb.py
@@ -1,17 +1,19 @@
-import sqlite3
-import os
+import db
 
-proj_root = os.path.join('..', os.path.dirname(__file__))
+#proj_root = os.path.join('..', os.path.dirname(__file__))
 
 if __name__ == '__main__':
     tables = {'metadata' : 'video_id, title, fps, abr, resolution, keywords, itag, mime_type, full_length_sec, captions, start_time, end_time'}
+    db_conn = db.DatabaseConnector(['metadata'])
     for k, v in tables.items():
-        db = os.path.join(proj_root, 'db', k+'.db')
-        conn = sqlite3.connect(db)
-        c = conn.cursor()
-        try:
-            c.execute('''CREATE TABLE {} ({})'''.format(k, v))
-        except sqlite3.Error as e:
-            print('Error occured: ', e.args[0])
-        conn.commit()
-        conn.close()
+        db_name = k
+        db_conn.init_db_table(db_name, k, v)
+        #db = os.path.join(proj_root, 'db', k+'.db')
+        #conn = sqlite3.connect(db)
+        #c = conn.cursor()
+        #try:
+        #    c.execute('''CREATE TABLE {} ({})'''.format(k, v))
+        #except sqlite3.Error as e:
+        #    print('Error occured: ', e.args[0])
+        #conn.commit()
+        #conn.close()
